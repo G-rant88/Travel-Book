614ae077cb6957c9293f897e1c0b2f41cd7a8954
Luis Mendoza <luismendoza@Luiss-MacBook-Pro.local>
2017-12-13 19:22:39 -0800

    added test files for uploading images

---
+++ upload_test.js
@@ -0,0 +1,41 @@
+var express = require('express'),
+    aws = require('aws-sdk'),
+    bodyParser = require('body-parser'),
+    multer = require('multer'),
+    multerS3 = require('multer-s3');
+
+aws.config.update({
+    secretAccessKey: '5e2GZFOWVhdlKJkAe76A7yscm5kCncwnXq7RBsBm',
+    accessKeyId: 'AKIAIPRKZVUFQBWEHWJQ',
+    region: 'us-west-1'
+});
+
+var app = express(),
+    s3 = new aws.S3();
+
+app.use(bodyParser.json());
+
+var upload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: 'travelbookpictures',
+        key: function (req, file, cb) {
+            console.log(file);
+            cb(null, file.originalname); //use Date.now() for unique file keys
+        }
+    })
+});
+
+//open in browser to see upload form
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/upload.html');
+});
+
+//used by upload form
+app.post('/upload', upload.array('upl',1), function (req, res, next) {
+    res.send("Uploaded!");
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
