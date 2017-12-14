2cdd1bd2cbc8397a527650d78d2c0698bf842fe1
Ben Grant <Benjah88@gmail.com>
2017-12-14 15:09:22 -0800

    results done

--- server.js
+++ server.js
@@ -24,7 +24,6 @@ require("./routes/api-routes.js")(app);
 require("./routes/html-routes.js")(app);
 
 db.sequelize.sync({
-	force: true
 }).then(function() {
 	app.listen(PORT, function() {
 		console.log("Travel App listening on PORT " + PORT);
