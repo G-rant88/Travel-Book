a7d5e9c956cc91c191658081ffcbece45a1bc931
Ben Grant <Benjah88@gmail.com>
2017-12-13 17:13:09 -0800

    login added

--- server.js
+++ server.js
@@ -8,10 +8,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 app.use(express.static("public"));
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 
 
+require("./routes/api-routes.js")(app);
 
+require("./routes/html-routes.js")(app);
 
 
 db.sequelize.sync({ force: true }).then(function() {
