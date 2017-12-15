12fc8d685905de78e821f8e7a22a9cb2284c985b
Diana <dianaltecson@gmail.com>
2017-12-15 11:37:05 -0800

    consolidated list view and submit buttons

--- public/javascript/results.js
+++ public/javascript/results.js
@@ -45,7 +45,61 @@ $(function () {
             $(this).attr('data-status', 'unliked');
             $(this).html('<i class="material-icons heart">favorite_border</i>');
             // remove result from liked list based on the post id
-            $('li[data-post-id="' + resultInfo.id + '"]').remove();
+            $('#slide-out li[data-post-id="' + resultInfo.id + '"]').remove();
         }
     });
+
+    
+    $('.add-btn').click(function (event) {
+        event.preventDefault();
+        // data associated with result post
+        var resultInfo = {
+            id: $(this).attr('data-post-id'),
+            name: $(this).attr('data-name'),
+            price: $(this).attr('data-price'),
+            rating: $(this).attr('data-rating'),
+            categories: $(this).attr('data-categories')
+        }
+        // changes heart icon from unadded to added
+        // adds result data to added list
+        if ($(this).attr('data-status') === 'unadded') {
+            $(this).attr('data-status', 'added');
+            $(this).html('<i class="material-icons heart">add_circle</i>');
+            // add result to added list
+            // display dollar signs according to price scale
+            var dollarSigns = '';
+            for (var i=0; i < resultInfo.price; i++) {
+                dollarSigns += '$';
+            }
+            // display rating according to rating scale
+            var stars = '';
+            var count = 0;
+            // whole stars
+            for (var i=0; i < resultInfo.rating; i++) {
+                count++;
+                stars += '<i class="material-icons">star</i>';
+            }
+            // fill in remaining scale with empty stars
+            if (count < 5) {
+                for (var i=0; i < (5-count); i++) {
+                    stars += '<i class="material-icons">star_border</i>';
+                }
+            }
+            var resultItem = 'Name: ' + resultInfo.name + '<br>' + 'Price: ' + dollarSigns + '<br>' + 'Rating: ' + stars + '<br>' + 'Category: ' + resultInfo.categories + '<br>';
+            $('#slide-right').append('<li  data-post-id="' + resultInfo.id + '">' + resultItem + '</li><hr>');
+        }
+        // change result from added to unadded
+        // removes item from added list
+        else {
+            $(this).attr('data-status', 'unadded');
+            $(this).html('<i class="material-icons heart">add_circle_outline</i>');
+            // remove result from added list based on the post id
+            $('#slide-right li[data-post-id="' + resultInfo.id + '"]').remove();
+        }
+    });
+
+
+
+    // upon submitting the liked list, post req to store in db
+
 });
