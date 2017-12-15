aabead3159312fb998b8af8aefc5e2e862bbbeb1
Diana <dianaltecson@gmail.com>
2017-12-15 10:12:56 -0800

    removing item for liked list

--- public/javascript/results.js
+++ public/javascript/results.js
@@ -1,22 +1,22 @@
 $(function () {
     $(".button-collapse").sideNav();
 
-    // changes heart icon from unliked to like, vice versa
     $('.like-btn').click(function (event) {
         event.preventDefault();
-
+        // data associated with result post
+        var resultInfo = {
+            id: $(this).attr('data-post-id'),
+            name: $(this).attr('data-name'),
+            price: $(this).attr('data-price'),
+            rating: $(this).attr('data-rating'),
+            categories: $(this).attr('data-categories')
+        }
+        // changes heart icon from unliked to like
+        // adds result data to liked list
         if ($(this).attr('data-status') === 'unliked') {
             $(this).attr('data-status', 'liked');
             $(this).html('<i class="material-icons heart">favorite</i>');
             // add result to liked list
-                // stored data to display in 
-            var resultInfo = {
-                id: $(this).attr('data-post-id'),
-                name: $(this).attr('data-name'),
-                price: $(this).attr('data-price'),
-                rating: $(this).attr('data-rating'),
-                categories: $(this).attr('data-categories')
-            }
             // display dollar signs according to price scale
             var dollarSigns = '';
             for (var i=0; i < resultInfo.price; i++) {
@@ -39,10 +39,14 @@ $(function () {
             var resultItem = 'Name: ' + resultInfo.name + '<br>' + 'Price: ' + dollarSigns + '<br>' + 'Rating: ' + stars + '<br>' + 'Category: ' + resultInfo.categories + '<br>';
             $('#slide-out').append('<li  data-post-id="' + resultInfo.id + '">' + resultItem + '</li><hr>');
         }
+        // change result from liked to unlike
+        // removes item from liked list
         else {
             $(this).attr('data-status', 'unliked');
             $(this).html('<i class="material-icons heart">favorite_border</i>');
             // remove result from liked list
+            // iterate through liked list and find where the button
+            // console.log('' + $('li').find('data-post-id=' + resultInfo.id));
         }
     });
 });
