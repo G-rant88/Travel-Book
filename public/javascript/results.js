8d0e1fdf2e62359b86d461e97344980af4a2ba14
Diana <dianaltecson@gmail.com>
2017-12-15 08:56:06 -0800

    able to add a result item to list for future trips

--- public/javascript/results.js
+++ public/javascript/results.js
@@ -8,10 +8,27 @@ $(function () {
         if ($(this).attr('data-status') === 'unliked') {
             $(this).attr('data-status', 'liked');
             $(this).html('<i class="material-icons heart">favorite</i>');
+            // add result to liked list
+            var resultInfo = {
+                name: $(this).attr('data-name'),
+                price: $(this).attr('data-price'),
+                rating: $(this).attr('data-rating'),
+                categories: $(this).attr('data-categories')
+            }
+            // display dollar signs according to price scale
+            var dollarSigns = '';
+            for (var i=0; i < resultInfo.price; i++) {
+                dollarSigns += '$';
+            }
+            var name = $(this).attr('data-name');
+            var resultItem = 'Name: ' + resultInfo.name + '<br>' + 'Price: ' + dollarSigns + '<br>' + 'Rating: ' + resultInfo.rating + '<br>' + 'Category: ' + resultInfo.categories + '<br>';
+            console.log(resultItem);
+            $('#slide-out').append('<li>' + resultItem + '</li><hr>');
         }
         else {
             $(this).attr('data-status', 'unliked');
             $(this).html('<i class="material-icons heart">favorite_border</i>');
+            // remove result from liked list
         }
-    })
+    });
 });
