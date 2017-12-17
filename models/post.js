fe52326b8f1194656ec28369b927f48185e503a8
Ben Grant <Benjah88@gmail.com>
2017-12-17 15:35:18 -0800

    big changes

--- models/post.js
+++ models/post.js
@@ -8,9 +8,7 @@ module.exports = function(sequelize, DataTypes) {
     categories: {type:DataTypes.STRING, allowNull:false},
     image: DataTypes.STRING,
     price: {type: DataTypes.INTEGER, defaultValue: 2, len: [1,3]},
-    rating: {type: DataTypes.INTEGER, defaultValue: 3, len: [1,5]},
-    past: DataTypes.STRING,
-    future: DataTypes.STRING
+    rating: {type: DataTypes.INTEGER, defaultValue: 3, len: [1,5]}
     
   });
 
