90411fdb260fa679b6d55063f5c95347e76221bd
Ben Grant <Benjah88@gmail.com>
2017-12-17 18:19:01 -0800

    more big changes

--- public/javascript/results.js
+++ public/javascript/results.js
@@ -1,212 +1,109 @@
 $(function () {
     // sidenav functionality
-    // $(".button-collapse").sideNav();
+    $(".button-collapse").sideNav();
 
     // modal functionality
-    // $('.modal').modal();
+    $('.modal').modal();
 
     $('.like-btn').click(function (event) {
         event.preventDefault();
         // data associated with result post
-        // var resultInfo = {
-        //     id: $(this).attr('data-post-id'),
-        //     name: $(this).attr('data-name'),
-        //     price: $(this).attr('data-price'),
-        //     rating: $(this).attr('data-rating'),
-        //     categories: $(this).attr('data-categories')
-        // }
-
+        var resultInfo = {
+            id: $(this).attr('data-post-id'),
+            name: $(this).attr('data-name'),
+            price: $(this).attr('data-price'),
+            rating: $(this).attr('data-rating'),
+            categories: $(this).attr('data-categories')
+        }
         // changes heart icon from unliked to like
         // adds result data to liked list
         if ($(this).attr('data-status') === 'unliked') {
             $(this).attr('data-status', 'liked');
             $(this).html('<i class="material-icons heart">favorite</i>');
+            // add result to liked list
+            // display dollar signs according to price scale
+            var dollarSigns = '';
+            for (var i=0; i < resultInfo.price; i++) {
+                dollarSigns += '$';
+            }
+            // display rating according to rating scale
+            var stars = '';
+            var count = 0;
+            // whole stars
+            for (var i=0; i < resultInfo.rating; i++) {
+                count++;
+                stars += '<i class="material-icons">star</i>';
+            }
+            // fill in remaining scale with empty stars
+            if (count < 5) {
+                for (var i=0; i < (5-count); i++) {
+                    stars += '<i class="material-icons">star_border</i>';
+                }
+            }
+            var resultItem = 'Name: ' + resultInfo.name + '<br>' + 'Price: ' + dollarSigns + '<br>' + 'Rating: ' + stars + '<br>' + 'Category: ' + resultInfo.categories + '<br>';
+            $('#slide-out').append('<li  class="future" data-post-id="' + resultInfo.id + '">' + resultItem + '</li><hr>');
         }
+        // change result from liked to unlike
+        // removes item from liked list
+        else {
+            $(this).attr('data-status', 'unliked');
+            $(this).html('<i class="material-icons heart">favorite_border</i>');
+            // remove result from liked list based on the post id
+            $('#slide-out li[data-post-id="' + resultInfo.id + '"]').remove();
+        }
+    });
+
+    
+   
+    });
+
+    // upon submitting the liked list, post req to store in db
+    $('.submit-list-btn').click(function (event) {
+        // prevent refresh of page
+        event.preventDefault();
+        
+        // names for trips
+        var previousTripName = $('.previous-trip-name').val();
+       
+        
+        // separate data for items for previous trips and future trips
+        var previousItems = [];
+
+
+        // add all items user wants to add to previous trip to specified 'previous' list
+        $('.previous').each(function () {
+            previousItems.push($(this).attr('data-post-id'));
+        });
 
         var cook = Cookies.get('name');
-        var city = $(".holder").attr("data-city");
-        var country = $(".holder").attr("data-country");
-        var place = $(this).attr('data-name');
-         var price = $(this).attr('data-price');
-         var rating = $(this).attr('data-rating');
-         var categories = $(this).attr('data-categories');
-         var review = $(this).attr('data-review');
-         var image = $(this).attr('data-image');
 
 
-        var info = {
-
+        var previousTrip = {
+            trip: previousTripName,
             user: cook,
-            country: country,
-            city: city,
-            place: place,
-            price: price,
-            rating: rating,
-            category: categories,
-            review: review,
-            image: image
-            
-        }
+            results: previousItems
+        };
 
 
-  $.ajax({
+        console.log(previousTrip);
+
+    
+        // // post request to update previous trip
+        $.ajax({
             method: 'POST',
-            url: '/favorite',
-            data: info,
+            url: '/add/trip',
+            data: previousTrip,
             success: function () {
-                console.log("added favorite");
-                Materialize.toast('Added to Travel Book!', 80000)
+                console.log('Updated trip');  
 
             }
         });
+            location.reload(); 
+    });
+
+ $('.edit-btn').click(function (event) {
+
+    var cook = Cookies.get('name');
+    location.assign("/edit/"+cook);
 
 });
-        
-});
-//             // add result to liked list
-//             // display dollar signs according to price scale
-//             var dollarSigns = '';
-//             for (var i=0; i < resultInfo.price; i++) {
-//                 dollarSigns += '$';
-//             }
-//             // display rating according to rating scale
-//             var stars = '';
-//             var count = 0;
-//             // whole stars
-//             for (var i=0; i < resultInfo.rating; i++) {
-//                 count++;
-//                 stars += '<i class="material-icons">star</i>';
-//             }
-//             // fill in remaining scale with empty stars
-//             if (count < 5) {
-//                 for (var i=0; i < (5-count); i++) {
-//                     stars += '<i class="material-icons">star_border</i>';
-//                 }
-//             }
-//             var resultItem = 'Name: ' + resultInfo.name + '<br>' + 'Price: ' + dollarSigns + '<br>' + 'Rating: ' + stars + '<br>' + 'Category: ' + resultInfo.categories + '<br>';
-//             $('#slide-out').append('<li  class="future" data-post-id="' + resultInfo.id + '">' + resultItem + '</li><hr>');
-//         }
-//         // change result from liked to unlike
-//         // removes item from liked list
-//         else {
-//             $(this).attr('data-status', 'unliked');
-//             $(this).html('<i class="material-icons heart">favorite_border</i>');
-//             // remove result from liked list based on the post id
-//             $('#slide-out li[data-post-id="' + resultInfo.id + '"]').remove();
-//         }
-//     });
-
-    
-//     $('.add-btn').click(function (event) {
-//         event.preventDefault();
-//         // data associated with result post
-//         var resultInfo = {
-//             id: $(this).attr('data-post-id'),
-//             name: $(this).attr('data-name'),
-//             price: $(this).attr('data-price'),
-//             rating: $(this).attr('data-rating'),
-//             categories: $(this).attr('data-categories')
-//         }
-//         // changes heart icon from unadded to added
-//         // adds result data to added list
-//         if ($(this).attr('data-status') === 'unadded') {
-//             $(this).attr('data-status', 'added');
-//             $(this).html('<i class="material-icons heart">add_circle</i>');
-//             // add result to added list
-//             // display dollar signs according to price scale
-//             var dollarSigns = '';
-//             for (var i=0; i < resultInfo.price; i++) {
-//                 dollarSigns += '$';
-//             }
-//             // display rating according to rating scale
-//             var stars = '';
-//             var count = 0;
-//             // whole stars
-//             for (var i=0; i < resultInfo.rating; i++) {
-//                 count++;
-//                 stars += '<i class="material-icons">star</i>';
-//             }
-//             // fill in remaining scale with empty stars
-//             if (count < 5) {
-//                 for (var i=0; i < (5-count); i++) {
-//                     stars += '<i class="material-icons">star_border</i>';
-//                 }
-//             }
-//             var resultItem = 'Name: ' + resultInfo.name + '<br>' + 'Price: ' + dollarSigns + '<br>' + 'Rating: ' + stars + '<br>' + 'Category: ' + resultInfo.categories + '<br>';
-//             $('#slide-right').append('<li  class="previous" data-post-id="' + resultInfo.id + '">' + resultItem + '</li><hr>');
-//         }
-//         // change result from added to unadded
-//         // removes item from added list
-//         else {
-//             $(this).attr('data-status', 'unadded');
-//             $(this).html('<i class="material-icons heart">add_circle_outline</i>');
-//             // remove result from added list based on the post id
-//             $('#slide-right li[data-post-id="' + resultInfo.id + '"]').remove();
-//         }
-//     });
-
-//     // upon submitting the liked list, post req to store in db
-//     $('#submit-list-btn').click(function (event) {
-//         // prevent refresh of page
-//         event.preventDefault();
-        
-//         // names for trips
-//         var previousTripName = $('.previous-trip-name').val();
-//         var futureTripName = $('.future-trip-name').val();
-        
-//         // separate data for items for previous trips and future trips
-//         var previousItems = [];
-//         var futureItems = [];
-
-//         // add all items user wants to add to previous trip to specified 'previous' list
-//         $('.previous').each(function () {
-//             previousItems.push($(this).attr('data-post-id'));
-//         });
-
-//         // add all items user wants to add to future trip to specified 'future' list
-//         $('.future').each(function () {
-//             futureItems.push($(this).attr('data-post-id'));
-//         });
-
-//         var cook = Cookies.get('name');
-
-//         var previousTrip = {
-//             name: previousTripName,
-//             results: previousItems,
-//             user: cook
-//         };
-
-//         var futureTrip = {
-//             name: futureTripName,
-//             results: futureItems,
-//             user: cook
-//         };
-
-//         console.log(previousTrip);
-//         console.log(futureTrip);
-
-//         // check if trip already exists
-
-//         // post request to add new trip to user acc
-//         $.ajax({
-//             method: 'POST',
-//             url: '/add/newtrip',
-//             data: futureTrip,
-//             success: function () {
-//                 console.log('Created new trip');
-//             }
-//         });
-
-//         // // post request to update previous trip
-//         $.ajax({
-//             method: 'POST',
-//             url: '/add/previoustrip',
-//             data: previousTrip,
-//             success: function () {
-//                 console.log('Updated past trip');                
-//             }
-//         });
-
-//         location.assign("/");
-//     });
-// });
