bbc395b9753b18c4653f6e8c43693b825140dc56
Diana <dianaltecson@gmail.com>
2017-12-18 22:12:27 -0800

    able to remove data if deselected

--- public/javascript/myPost.js
+++ public/javascript/myPost.js
@@ -6,22 +6,27 @@
     var data = {
       tripName: $(this).attr('data-trip-name')
     }
-      $(this).attr('data-status', 'open');
+    console.log($(this).hasClass('active'));
+    if (!$(this).hasClass('active')) {
       $.ajax({
         method: 'GET',
         url: '/future/' + cook + '/' + data.tripName,
         success: function (data){
           
           // attach data to div
-          var allPosts = $('<div>');
+          var allPosts = $('<div class="trip-info">');
           for (var i=0; i < data.tripPosts.length; i++) {
             allPosts.append('<p>' + data.tripPosts[i].name + '</p><hr>');
           }
+          // append data to active list item
           $('li.active .collapsible-body').append(allPosts);
-          //console.log(allPosts);
-          //location.reload();
+          
         }
       });
+    }
+    else {
+      $('.trip-info').remove();
+    }
   })
   
   $('.clear-btn').click(function (event) {
